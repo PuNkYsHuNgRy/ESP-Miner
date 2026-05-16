83eaa4d602aa8277d05df5710eb5cd4799bf8b2f
PuNkYsHuNgRy <punkyshungry@users.noreply.github.com>
2026-05-16 18:31:19 +0000

    remove version mismatch warning from web UI

--- main/http_server/axe-os/src/app/components/home/home.component.ts
+++ main/http_server/axe-os/src/app/components/home/home.component.ts
@@ -971,7 +971,7 @@ export class HomeComponent implements OnInit, OnDestroy {
     updateMessage(!!info.hardware_fault, 'HARDWARE_FAULT', 'error', `${info.hardware_fault}`);
     updateMessage(!info.frequency || info.frequency < 400, 'FREQUENCY_LOW', 'warn', 'Device frequency is set low - See settings');
     updateMessage(!!info.isUsingFallbackStratum, 'FALLBACK_STRATUM', 'warn', 'Using fallback pool - Share stats reset. Check Pool Settings and / or reboot Device.');
-    updateMessage(info.version !== info.axeOSVersion, 'VERSION_MISMATCH', 'warn', `Firmware (${info.version}) and AxeOS (${info.axeOSVersion}) versions do not match. Please make sure to update both www.bin and esp-miner.bin.`);
+    // Version mismatch removed - normal after OTA, web UI updates separately
     if (info.coinbaseOutputs && info.coinbaseOutputs?.length > 0) {
       let percentage = this.getPayoutPercentage(info);
       updateMessage(percentage > 0 && percentage < 95, 'NOT_SOLO_MINING', 'warn', `Your share of the mining reward is only ${percentage.toFixed(1)}%`);
